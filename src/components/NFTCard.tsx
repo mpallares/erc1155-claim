73a910fb3fd92b9ca58f957854f994261558b76a
mpallares <mpallareslara@gmail.com>
2025-09-26 16:05:00 +0700

    Add NFT card component

---
+++ src/components/NFTCard.tsx
@@ -0,0 +1,30 @@
+
+import { NFT } from '@/types/nft';
+import { convertIpfsUrl } from '@/lib/ipfs';
+import Image from 'next/image';
+
+interface NFTCardProps {
+  nft: NFT;
+}
+
+export function NFTCard({ nft }: NFTCardProps) {
+
+  return (
+    <div className='w-[280px] bg-white flex flex-col gap-2'>
+      <div className='aspect-square relative'>
+        <Image
+          src={convertIpfsUrl(nft.metadata.image)}
+          alt={nft.metadata.name}
+          fill
+          className='object-cover'
+        />
+      </div>
+      <div>
+        <h3 className='font-semibold text-dark text-md leading-6'>
+          {nft.metadata.name}
+        </h3>
+        <p className='text-gray-500 text-sm leading-5 font-normal'>0.0 ETH</p>
+      </div>
+    </div>
+  );
+}
