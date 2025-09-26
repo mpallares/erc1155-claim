95079ad7f9f616810c9ea8a6fb86d4a8e4660b31
mpallares <mpallareslara@gmail.com>
2025-09-27 04:41:15 +0700

    Initial server data and then refetches from client to API

--- src/components/NFTContent.tsx
+++ src/components/NFTContent.tsx
@@ -4,27 +4,33 @@ import { useState } from 'react';
 import { NFTDetails } from '@/components/NFTDetails';
 import { NFTList } from '@/components/NFTList';
 import { NFT } from '@/types/nft';
+import { useNFTs } from '@/hooks/useNFTs';
 
 interface NFTContentProps {
   nfts: NFT[];
 }
 
-export default function NFTContent({ nfts }: NFTContentProps) {
-  const [selectedNFT, setSelectedNFT] = useState<NFT>(nfts[0]);
+export default function NFTContent({ nfts: initialNfts }: NFTContentProps) {
+  const { data: nfts = [], isError } = useNFTs(initialNfts);
+  const [selectedNFTId, setSelectedNFTId] = useState<string | null>(null);
 
-  const remainingNFTs = nfts.filter(nft => nft.id !== selectedNFT?.id);
+  const selectedNFT = selectedNFTId
+    ? nfts.find((nft) => nft.id === selectedNFTId) || nfts[0]
+    : nfts[0];
+
+  const remainingNFTs = nfts.filter((nft) => nft.id !== selectedNFT?.id);
 
   const handleNFTSelect = (nft: NFT) => {
-    setSelectedNFT(nft);
+    setSelectedNFTId(nft.id);
   };
+  if (isError) {
+    return <div className='text-red-800 p-4'>Failed to load NFTs</div>;
+  }
 
   return (
     <>
       {selectedNFT && <NFTDetails nft={selectedNFT} />}
-      <NFTList
-        nfts={remainingNFTs}
-        onNFTClick={handleNFTSelect}
-      />
+      <NFTList nfts={remainingNFTs} onNFTClick={handleNFTSelect} />
     </>
   );
 }
