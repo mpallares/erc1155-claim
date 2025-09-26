c0b67790118c188cf200787582c9287e12ce1f13
mpallares <mpallareslara@gmail.com>
2025-09-26 14:46:15 +0700

    Add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import KilnLogo from '@/../public/icons/kiln-logo.svg';
+
+export function Navbar() {
+  return (
+    <nav className="flex justify-between items-center w-full px-6 py-5 bg-white">
+      <div className="flex items-center">
+        <KilnLogo />
+      </div>
+      <button className="bg-secondary text-gray-50 px-4 py-2 hover:bg-secondary/85 transition-colors font-medium text-md leading-5 tracking-normal cursor-pointer shadow-button">
+        Connect Wallet
+      </button>
+    </nav>
+  );
+}
