3b5ec7b702c672b341d9762e82c5512164112310
mpallares <mpallareslara@gmail.com>
2025-09-28 04:25:18 +0700

    Clean up ABI

--- src/abis/erc1155.ts
+++ src/abis/erc1155.ts
@@ -2,25 +2,57 @@ export const ERC1155_ABI = [
   {
     name: 'claim',
     type: 'function',
-    stateMutability: 'nonpayable',
-    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
-    outputs: [],
+    stateMutability: 'payable',
+    inputs: [
+      { name: '_receiver', type: 'address' },
+      { name: '_tokenId', type: 'uint256' },
+      { name: '_quantity', type: 'uint256' },
+      { name: '_currency', type: 'address' },
+      { name: '_pricePerToken', type: 'uint256' },
+      {
+        name: '_allowlistProof',
+        type: 'tuple',
+        components: [
+          { name: 'proof', type: 'bytes32[]' },
+          { name: 'quantityLimitPerWallet', type: 'uint256' },
+          { name: 'pricePerToken', type: 'uint256' },
+          { name: 'currency', type: 'address' }
+        ]
+      },
+      { name: '_data', type: 'bytes' }
+    ],
+    outputs: []
   },
   {
-    name: 'balanceOf',
+    name: 'getActiveClaimConditionId',
+    type: 'function',
+    stateMutability: 'view',
+    inputs: [{ name: '_tokenId', type: 'uint256' }],
+    outputs: [{ type: 'uint256' }]
+  },
+  {
+    name: 'getClaimConditionById',
     type: 'function',
     stateMutability: 'view',
     inputs: [
-      { internalType: 'address', name: 'account', type: 'address' },
-      { internalType: 'uint256', name: 'id', type: 'uint256' }
+      { name: '_tokenId', type: 'uint256' },
+      { name: '_conditionId', type: 'uint256' }
     ],
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-  },
-  {
-    name: 'totalSupply',
-    type: 'function',
-    stateMutability: 'view',
-    inputs: [{ internalType: 'uint256', name: 'id', type: 'uint256' }],
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-  },
+    outputs: [
+      {
+        name: 'condition',
+        type: 'tuple',
+        components: [
+          { name: 'startTimestamp', type: 'uint256' },
+          { name: 'maxClaimableSupply', type: 'uint256' },
+          { name: 'supplyClaimed', type: 'uint256' },
+          { name: 'quantityLimitPerWallet', type: 'uint256' },
+          { name: 'merkleRoot', type: 'bytes32' },
+          { name: 'pricePerToken', type: 'uint256' },
+          { name: 'currency', type: 'address' },
+          { name: 'metadata', type: 'string' }
+        ]
+      }
+    ]
+  }
 ] as const;
